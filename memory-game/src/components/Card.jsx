49b00045e6751a256ad9bc2df52d6d567af88055
maayan3330 <maayan3330@gmail.com>
2026-03-08 20:55:32 +0200

    add the state for filp card

--- memory-game/src/components/Card.jsx
+++ memory-game/src/components/Card.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {useState} from React
+import {useState} from 'react';
+import CLOSE from '../assets/flipcard.png';
 // The Card component goal:
 // 1 - present a diff img every time
 // 2 - be able to flip them on click
@@ -8,9 +8,24 @@ import {useState} from React
 export default function Card({image}) {
     // at the beiging the card close => false
     const [isFlipped, setIsFlipped] = useState(false);
+
+    // function to handle filp on click
+    function handleFlip() {
+        setIsFlipped(!isFlipped);
+    }
     return(
-        <div className="card">
-            <img src={image} alt="character"/>
+        <div className="card" onClick={handleFlip}>
+        <div className={isFlipped ? "card-inner flipped" : "card-inner"}>
+            
+            <div className="card-front">
+            <img src={image} />
+            </div>
+
+            <div className="card-back">
+            <img src={CLOSE} />
+            </div>
+
+        </div>
         </div>
     );
 }
